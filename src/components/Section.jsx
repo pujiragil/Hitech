32494933cb68d91dd9d41f57d14035ae5e9d374d
pujiragil <ragilsg164@gmail.com>
2022-08-01 21:10:15 +0700

    fixed image section size

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -9,9 +9,9 @@ const Section = () => {
             <p className="medium:text-lg text-base w-11/12 mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt molestiae aliquam exercitationem animi tempore natus iusto delectus</p>
             <button className="bg-zinc-900 text-base hover:bg-zinc-700 duration-300 font-bold w-fit text-white medium:px-10 px-9 medium:py-4 py-3 rounded-full drop-shadow-xl">See More</button>
           </div>
-          <div className="flex relative">
-            <div className="before:-top-4 before:-left-4 before:border-2 before:border-zinc-900 before:w-full before:h-full before:absolute">
-              <img className="relative grayscale cursor-pointer hover:grayscale-0 duration-300 w-fit" src="https://images.unsplash.com/photo-1590959651373-a3db0f38a961?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=739&q=80" alt="" />
+          <div className="flex relative justify-center items-center">
+            <div className="medium:before:-top-6 medium:before:w-full medium:before:-left-6 before:-top-4 before:-right-1 before:left-1 before:bottom-1 before:border-2 before:border-zinc-900 before:w-10/12 before:h-full before:absolute w-10/12 medium:w-full">
+              <img className="relative grayscale cursor-pointer hover:grayscale-0 duration-300 w-full" src="https://images.unsplash.com/photo-1590959651373-a3db0f38a961?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=739&q=80" alt="" />
             </div>
           </div>
         </div>
