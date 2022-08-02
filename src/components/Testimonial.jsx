9871470685931597489cc869efe5081ad18fcbf2
pujiragil <ragilsg164@gmail.com>
2022-08-02 22:40:22 +0700

    adding responsive style for desktop view

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Testi from './fragments/testi'
+import { obj1, obj2, obj3, obj4, obj5, obj6 } from './fragments/testiObj'
 
 const Testimonial = () => {
   return (
@@ -7,9 +8,18 @@ const Testimonial = () => {
       <div className="flex flex-col">
         <h1 className="text-center text-3xl text-zinc-900 font-medium py-10">Our Testimonials</h1>
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
-          <Testi />
-          <Testi />
-          <Testi />
+          <div className="flex flex-col items-center gap-y-6 mb-8">
+            <Testi {...obj1} />
+            <Testi {...obj2} />
+          </div>
+          <div className="flex flex-col items-center gap-y-6 mb-8">
+            <Testi {...obj3} />
+            <Testi {...obj4} />
+          </div>
+          <div className="flex flex-col items-center gap-y-6 mb-8">
+            <Testi {...obj5} />
+            <Testi {...obj6} />
+          </div>
         </div>
       </div>
     </div>
---
+++ src/components/fragments/testiObj.jsx
@@ -0,0 +1,41 @@
+export const obj1 = {
+  comment: "Sint qui tempor officia Lorem Lorem. Occaecat incididunt nostrud quis mollit incididunt.",
+  name: "Joko Banteng",
+  job: "Presiden Wakanda",
+  imgLink: "https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop"
+}
+
+export const obj2 = {
+  comment: "Tempor voluptate dolore fugiat duis cupidatat excepteur sit cupidatat sint. Occaecat et commodo sit cupidatat elit anim ad adipisicing. Tempor voluptate dolore fugiat duis cupidatat excepteur sit cupidatat sint. Occaecat et commodo sit cupidatat elit anim ad adipisicing",
+  name: "Megawati Kebo",
+  job: "Penakluk Banteng",
+  imgLink: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8cGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop"
+}
+
+export const obj3 = {
+  comment: "Ex consectetur id cillum esse occaecat consectetur exercitation culpa Lorem ex officia velit irure. Ullamco magna magna in ipsum occaecat dolore est anim ut nostrud. Proident ad velit in anim pariatur reprehenderit ullamco consectetur sunt.",
+  name: "Pudidi Pudidi",
+  job: "Yungtuber",
+  imgLink: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop"
+}
+
+export const obj4 = {
+  comment: "Id cupidatat ea nostrud quis occaecat aute aliqua esse eiusmod in cillum ad ad elit. Mollit enim sunt do occaecat.",
+  name: "Puan Maharini",
+  job: "Penguasa Lembah",
+  imgLink: "https://images.unsplash.com/photo-1524504388940-b1c1722653e1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop"
+}
+
+export const obj5 = {
+  comment: "Tempor deserunt voluptate minim cillum mollit.",
+  name: "Dustin Tiffani",
+  job: "Sejarawan Yunani",
+  imgLink: "https://images.unsplash.com/photo-1542909168-82c3e7fdca5c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop"
+}
+
+export const obj6 = {
+  comment: "Velit eu est velit officia nostrud duis aliquip ut. Officia eu non aliquip veniam incididunt. Incididunt nisi officia laborum esse. Dolore pariatur consequat officia anim ipsum irure",
+  name: "Jeje Slebew",
+  job: "Model DBMS",
+  imgLink: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop"
+}
