3cec5140ac629a7f07c17abd49bb62676e45918b
pujiragil <ragilsg164@gmail.com>
2022-08-04 10:52:03 +0700

    fix bug at login & register page

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -6,7 +6,9 @@ const RegisterPage = () => {
   return (
     <div className="container mx-auto px-6">
       <div className="relative grid grid-cols-1 py-6 h-screen lg:h-hero">
-        <Link to="/" className="text-2xl font-roboto font-bold">Hitech</Link>
+        <div>
+          <Link to="/" className="text-2xl font-roboto font-bold">Hitech</Link>
+        </div>
         <div className="flex bg-zinc-900 flex-col text-white p-6 rounded-lg drop-shadow-2xl h-fit w-full md:w-8/12 lg:w-2/5 sm:w-9/12 justify-self-center">
           <h2 className="text-center text-3xl font-roboto mb-3">Register</h2>
           <form action="">
