4c9a1be6fe7bcd15ff2c09313d39149ee8086577
pujiragil <ragilsg164@gmail.com>
2022-08-04 10:47:31 +0700

    tambahin shadow kelupaan

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -7,7 +7,7 @@ const LoginPage = () => {
     <div className="container mx-auto px-6">
       <div className="relative grid grid-cols-1 py-6 h-screen">
         <Link to="/" className="text-2xl font-roboto font-bold">Hitech</Link>
-        <div className="flex bg-zinc-900 flex-col text-white p-6 rounded-lg h-fit w-full md:w-8/12 lg:w-2/5 sm:w-9/12 justify-self-center">
+        <div className="flex bg-zinc-900 flex-col text-white p-6 rounded-lg drop-shadow-2xl h-fit w-full md:w-8/12 lg:w-2/5 sm:w-9/12 justify-self-center">
           <h2 className="text-center text-3xl font-roboto mb-3">Login</h2>
           <form action="">
             <FieldForm labelFor="email" label="Email *" type="email" inputName="email" value="e.g johndoe@gmail.com" />
